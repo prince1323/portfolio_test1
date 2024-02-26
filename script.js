2b79d54214966357cb622fb028d340677c34e986
Prince Pandey <114833106+prince1323@users.noreply.github.com>
2024-02-27 01:12:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var navbarNav = document.getElementById("navbar-nav");
+    navbarNav.classList.toggle("active");
+
+    var menuIcon = document.querySelector(".menu-icon");
+    menuIcon.classList.toggle("active");
+}
